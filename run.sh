3757c6dfe1b1eb66223dc3003fc3034719cc1897
Tom Harrop <twharrop@gmail.com>
2024-12-16 15:09:09 +1100

    spartan setup

--- run.sh
+++ run.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
 #SBATCH --job-name=atol_test
-#SBATCH --time=2-00
+#SBATCH --time=0-01
 #SBATCH --ntasks=2
 #SBATCH --mem=8g
 #SBATCH --output=sm.slurm.out
 #SBATCH --error=sm.slurm.err
-#SBATCH --partition=interactive
 
 # Dependencies
 module load Apptainer/1.3.3
 
 # Application specific commands:
-printenv
-
-exit 1
-
-
-printf "JOBDIR: %s\n" "${JOBDIR}"
-printf "LOCALDIR: %s\n" "${LOCALDIR}"
-printf "MEMDIR: %s\n" "${MEMDIR}"
 printf "TMPDIR: %s\n" "${TMPDIR}"
 
 snakemake \
-	--profile petrichor_tmp \
+	-n \
+	--profile spartan_v8 \
 	--retries 0 \
 	--keep-going \
 	--cores 128 \

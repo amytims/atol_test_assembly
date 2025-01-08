20a95784230dcc62d57e7b25e5171f0cfef00812
Tom Harrop <twharrop@gmail.com>
2025-01-08 13:58:16 +1100

    start pawsey setup

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Test an assembly using the sanger-tol pipeline. Basically download the reads,
 reformat them to suit the pipeline, and generate the YAML file.
+
+## Notes
+
+- We can register Acacia using the Snakemake [S3
+  plugin](https://snakemake.github.io/snakemake-plugin-catalog/plugins/storage/s3.html).
+  This will be useful for the process that downloads the read files.
+- We should be able to load the files directly from Acacia into the DToL
+  workflow by configuring [S3 in
+  Nexflow](https://www.nextflow.io/docs/latest/amazons3.html#s3-compatible-storage).
---
+++ run_pawsey.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --job-name=atol_test
+#SBATCH --time=3-00
+#SBATCH --ntasks=2
+#SBATCH --mem=8g
+#SBATCH --output=sm.slurm.out
+#SBATCH --error=sm.slurm.err
+
+# Dependencies
+module load python/3.11.6
+module load singularity/4.1.0-nohost
+module load nextflow/24.04.3
+
+# Application specific commands:
+set -eux
+
+printf "TMPDIR: %s\n" "${TMPDIR}"
+
+if [ -z "${SINGULARITY_CACHEDIR}" ]; then
+	export SINGULARITY_CACHEDIR=/software/projects/pawsey1132/tharrop/.singularity
+fi
+
+export NXF_APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+export NXF_SINGULARITY_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+
+snakemake \
+	--profile profiles/pawsey_v8 \
+	--retries 0 \
+	--keep-going \
+	--cores 12 \
+	--local-cores 2
+
+# Pull the containers into the cache before trying to launch the workflow.
+nextflow inspect \
+	-concretize sanger-tol/genomeassembly \
+	--input results/config/sangertol_genomeassembly_params.yaml \
+	--outdir results/sanger_tol \
+	-profile singularity,pawsey \
+	-r 0.10.0
+
+# Note, it's tempting to use the apptainer profile, but the nf-core (and some
+# sanger-tol) pipelines have a conditional `workflow.containerEngine ==
+# 'singularity'` that prevents using the right URL with apptainer.
+nextflow \
+	-log "nextflow.$(date +"%Y%m%d%H%M%S").${RANDOM}.log" \
+	run \
+	sanger-tol/genomeassembly \
+	--input results/config/sangertol_genomeassembly_params.yaml \
+	--outdir results/sanger_tol \
+	-resume \
+	-profile singularity,pawsey \
+	-r 0.10.0
